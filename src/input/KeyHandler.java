d088bbdf796acc057aa8e3edce8fae854fafc971
Edmar <jose.fortyfive@gmail.com>
2023-10-01 09:37:23 +0800

    Updated the music in Game Over feature

--- src/input/KeyHandler.java
+++ src/input/KeyHandler.java
@@ -234,6 +234,7 @@ public class KeyHandler implements KeyListener {
             if(gp.ui.commandNum == 0){
                 gp.gameState = gp.playState;
                 gp.retry();
+                gp.playMusic(0);
             }
             else if(gp.ui.commandNum == 1){
                 gp.gameState = gp.titleState;
